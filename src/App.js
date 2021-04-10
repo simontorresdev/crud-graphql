e6237273092bd271fbe393adf168d022f25130d9
unknown <simontorres979797@gmail.com>
2021-04-10 11:09:42 -0500

    instalando dependecias necesarias para el proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import { useQuery, gql } from '@apollo/client'
+import './App.css'
 
-function App() {
+const GET_CLIENTS = gql`
+  query{
+    clientsSearch{
+      currentPage
+      totalPages
+      resultsPerPage
+      results{
+        id,
+        firstName
+        lastName
+        cedula
+        address
+        city
+        cellphone
+        credit
+      }
+    }
+  }
+`
+
+function App () {
+  const { loading, error, data } = useQuery(GET_CLIENTS)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      {console.log(data, 'data')}
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/graphql/client.js
@@ -0,0 +1,9 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+
+export const client = new ApolloClient({
+  uri: 'https://onboarding-redesign.dev.elenas.la/gql/',
+  cache: new InMemoryCache(),
+  headers: {
+    Authorization: 'Token eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiKzU3MzA1NzE5OTk5NSIsImlhdCI6MTYxODA2NTU2MH0.Tjk82iTgqop3RiuNQ_6gkRPi_enuFMknWDyoZna7jww'
+  }
+})
